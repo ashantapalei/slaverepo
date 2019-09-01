d369b25d7e710f62e65b4a87e8bcc60feb648f89
Ashanta Palei <paleikashantkumar@gmail.com>
2019-09-02 00:46:32 +0530

    ip address udpate

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 echo "===============Copying file from remote location======================"
 
 echo "================Update patch====================="
-ssh -tt 172.22.22.201 sudo apt-get update
+ssh -tt 172.22.22.162 sudo apt-get update
 echo "==================Apache install=================="
-ssh -tt 172.22.22.201 sudo apt-get install apache2 -y
+ssh -tt 172.22.22.162 sudo apt-get install apache2 -y
 echo "=====================Restart sevice========================"
-ssh -tt 172.22.22.201 sudo systemctl apache2 restart
+ssh -tt 172.22.22.162 sudo systemctl apache2 restart
 
